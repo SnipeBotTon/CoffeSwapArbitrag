e2b66ecd2f1ffad52846559bf6fd58ff757d957b
FermaVS <166220131+SnipeBotTon@users.noreply.github.com>
2024-07-21 12:14:45 +0500

    Update hlwalletcreate.py

--- hlwalletcreate.py
+++ hlwalletcreate.py
@@ -1,15 +1,13 @@
-import os
 import asyncio
 
-from pytoniq import LiteBalancer, WalletV4R2, LiteClient, HighloadWallet
+from pytoniq import LiteBalancer, HighloadWallet
 
 from tonsdk.crypto import mnemonic_new
-#from tonsdk.contract.wallet import WalletVersionEnum, Wallets
-from tonsdk.utils import Address, bytes_to_b64str, b64str_to_bytes, to_nano
+
 
 mnemonics = mnemonic_new()
 
-#_mnemonics, _pub_k, _priv_k, wallet = Wallets.from_mnemonics(    mnemonics, WalletVersionEnum('hv2'), 0)
+
 async def main():
     client = LiteBalancer.from_mainnet_config(trust_level=1)
     await client.start_up()
